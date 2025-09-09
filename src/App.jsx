4e6d6b3ec5a21f3a24dc37921103315a99369d90
petermagalong <petermagalong042198@gmail.com>
2025-09-09 18:10:45 -0500

    refresher course

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Quiz from './components/quiz'
+import './index.css'
+function App() {
+
+  return (
+    <div className='app-container '>
+      <h1>Hello</h1>
+      <Quiz/>
+    </div>
+  )
+}
+
+export default App
